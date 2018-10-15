7666c8a3bf3b6bf53f1d9a6dc74880cd2a93dedb
Shubham Gupta <shubham@Shubhams-MacBook-Pro.local>
2018-10-14 19:20:58 -0500

    added react-redux

--- src/ReactAuthorQuiz.js
+++ src/ReactAuthorQuiz.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import logo from './logo.svg';
 import './App.css';
@@ -70,16 +71,35 @@ function Footer() {
   </div>)
 }
 
-function ReactAuthorQuiz({turnData, highlight, onAnswerSelected, onContinue}) {
-  return (
-    <div className="container-fluid">
-      <Hero/>
-      <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected} />
-      <Continue show={highlight === 'correct'} onContinue={onContinue}/>
-      <p><Link to="/add">Add an author</Link></p>
-      <Footer/>
-    </div>
-  );
+function mapStateToProps(state) {
+  return {
+    turnData: state.turnData,
+    highlight: state.highlight
+  };
 }
+
+function mapDispatchToProps(dispatch) {
+  return{
+    onAnswerSelected: (answer) => {
+      dispatch({type: 'ANSWER_SELECTED', answer});
+    },
+    onContinue: () => {
+      dispatch({type: 'CONTINUE'});
+    }
+  };
+}
+
+const ReactAuthorQuiz = connect(mapStateToProps, mapDispatchToProps)(
+  function ReactAuthorQuiz({turnData, highlight, onAnswerSelected, onContinue}) {
+    return (
+      <div className="container-fluid">
+        <Hero/>
+        <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected} />
+        <Continue show={highlight === 'correct'} onContinue={onContinue}/>
+        <p><Link to="/add">Add an author</Link></p>
+        <Footer/>
+      </div>
+    );
+});
 
 export default ReactAuthorQuiz;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Route, withRouter} from 'react-router-dom';
+import * as Redux from 'redux';
+import * as ReactRedux from 'react-redux';
 import './index.css';
 import ReactAuthorQuiz from './ReactAuthorQuiz';
 import AddAuthorForm from './AddAuthorForm';
@@ -50,39 +52,48 @@ function getTurnData(authors) {
   }
 }
 
-function resetState() {
+/*function resetState() {
   return {
     turnData: getTurnData(authors),
     highlight: ''
   };
+} */
+
+function reducer(state = {authors, turnData: getTurnData(authors), highlight: ''}, action) {
+  switch (action.type) {
+    case 'ANSWER_SELECTED':
+      const isCorrect = state.turnData.author.books.some((book) => book === action.answer);
+      return Object.assign({}, state, {highlight: isCorrect ? 'correct' : 'wrong'});
+    case 'CONTINUE':
+      return Object.assign({}, state, {highlight: '', turnData: getTurnData(state.authors)});
+    case 'ADD_AUTHOR':
+      return Object.assign({}, state, {highlight: '', authors: state.authors.concat([action.author])});
+    default:
+      return state;
+  }
 }
 
-let state = resetState();
+//let state = resetState();
+let store = Redux.createStore(reducer);
 
+/*
 function onAnswerSelected(answer) {
   const isCorrect = state.turnData.author.books.some((book) => book === answer);
   state.highlight = isCorrect ? 'correct' : 'wrong';
   render();
-}
+} */
 
-const AuthorWrapper = withRouter(({history}) =>
-  <AddAuthorForm onAddAuthor={(author) => {
-    authors.push(author);
-    history.push('/');
-  }} />
-);
+ReactDOM.render(
+  <BrowserRouter>
+    <ReactRedux.Provider store={store}>
+      <React.Fragment>
+        <Route exact path="/" component={ReactAuthorQuiz} />
+        <Route path="/add" component={AddAuthorForm} />
+      </React.Fragment>
+    </ReactRedux.Provider>
+  </BrowserRouter>, document.getElementById('root'));
 
-function App() {
-  return <ReactAuthorQuiz {...state}
-    onAnswerSelected={onAnswerSelected}
-    onContinue={() => {
-      state = resetState();
-      render();
-    }}
-  />;
-}
-
-function render() {
+/*function render() {
   ReactDOM.render(
     <BrowserRouter>
       <React.Fragment>
@@ -92,6 +103,7 @@ function render() {
     </BrowserRouter>, document.getElementById('root'));
 }
 render();
+*/
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
